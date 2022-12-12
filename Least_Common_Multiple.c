28ade9bdc95ce4410462cd4f401d6d14c49c4c50
Yogeesha H C <hcyogeesh@gmail.com>
2022-12-12 09:39:07 +0530

    #include <stdio.h>
    int gcd(int a, int b)
    {
    	if (a == 0)
    		return b;
    	return gcd(b % a, a);
    }
    
    
    int lcm(int a, int b)
    {
    	return (a / gcd(a, b)) * b;
    }
    
    
    int main()
    {
    	int a, b;
    	scanf("%d%d", &a, &b);
    	printf("%d", lcm(a, b));
    	return 0;
    }

---
+++ Least_Common_Multiple.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int gcd(int a, int b)
+{
+	if (a == 0)
+		return b;
+	return gcd(b % a, a);
+}
+
+
+int lcm(int a, int b)
+{
+	return (a / gcd(a, b)) * b;
+}
+
+
+int main()
+{
+	int a, b;
+	scanf("%d%d", &a, &b);
+	printf("%d", lcm(a, b));
+	return 0;
+}
