91e55a39457d388c699d2bdb240220b616bfc9b6
Yogeesha H C <hcyogeesh@gmail.com>
2022-12-13 18:43:23 +0530

    #include <stdio.h>
    // Function to print the output
    void printTheArray(int arr[], int n)
    {
    	for (int i = 0; i < n; i++) {
    		printf("%d", arr[i]);
    	}
    	printf("
    ");
    }
    
    // Function to generate all binary strings
    void generateAllBinaryStrings(int n, int arr[], int i)
    {
    	if (i == n) {
    		printTheArray(arr, n);
    		return;
    	}
    
    	// First assign "0" at ith position
    	// and try for all other permutations
    	// for remaining positions
    	arr[i] = 0;
    	generateAllBinaryStrings(n, arr, i + 1);
    
    	// And then assign "1" at ith position
    	// and try for all other permutations
    	// for remaining positions
    	arr[i] = 1;
    	generateAllBinaryStrings(n, arr, i + 1);
    }
    
    // Driver Code
    int main()
    {
    	int n;
    	scanf("%d", &n);
    
    	int arr[n];
    
    	// Print all binary strings
    	generateAllBinaryStrings(n, arr, 0);
    
    	return 0;
    }

---
+++ Binary_Number.c
@@ -0,0 +1,49 @@
+
+
+#include <stdio.h>
+
+
+// Function to print the output
+void printTheArray(int arr[], int n)
+{
+	for (int i = 0; i < n; i++) {
+		printf("%d", arr[i]);
+	}
+	printf("
+");
+}
+
+// Function to generate all binary strings
+void generateAllBinaryStrings(int n, int arr[], int i)
+{
+	if (i == n) {
+		printTheArray(arr, n);
+		return;
+	}
+
+	// First assign "0" at ith position
+	// and try for all other permutations
+	// for remaining positions
+	arr[i] = 0;
+	generateAllBinaryStrings(n, arr, i + 1);
+
+	// And then assign "1" at ith position
+	// and try for all other permutations
+	// for remaining positions
+	arr[i] = 1;
+	generateAllBinaryStrings(n, arr, i + 1);
+}
+
+// Driver Code
+int main()
+{
+	int n;
+	scanf("%d", &n);
+
+	int arr[n];
+
+	// Print all binary strings
+	generateAllBinaryStrings(n, arr, 0);
+
+	return 0;
+}
