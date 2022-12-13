c7715c7b1b0b5e88f72bad2c6ee1296ff663e8b1
Yogeesha H C <hcyogeesh@gmail.com>
2022-12-13 21:10:43 +0530

    #include<stdio.h>
    int main(){
        
        int n, i, sum=0, avg, count=0;
        
        scanf("%d", &n);
        
        int a[n];
        
        for(i=0; i<n; i++){
            scanf("%d", &a[i]);
            sum+=a[i];
        }
        avg= sum/n;
        
        for(i=0; i<n; i++){
            if(a[i]>=avg)
                count++;
        }
        printf("%d", count);
        return 0;
    }

---
+++ elements_greater_than_average.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+    
+    int n, i, sum=0, avg, count=0;
+    
+    scanf("%d", &n);
+    
+    int a[n];
+    
+    for(i=0; i<n; i++){
+        scanf("%d", &a[i]);
+        sum+=a[i];
+    }
+    avg= sum/n;
+    
+    for(i=0; i<n; i++){
+        if(a[i]>=avg)
+            count++;
+    }
+    printf("%d", count);
+    return 0;
+}
