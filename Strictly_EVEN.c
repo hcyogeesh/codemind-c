dece6f4fcbe6294a5e994d58b52a8dd5bb3a7ee2
Yogeesha H C <hcyogeesh@gmail.com>
2022-12-23 20:25:43 +0530

    #include<stdio.h>
    int main(){
        
        int n, arr[100],i, flag=1;
        
        scanf("%d", &n);
        
        for(i=0;i<n;i++)
            scanf("%d", &arr[i]);
        for(i=0;i<n;i++){
            
            if(i%2==1 && arr[i]%2==0){
                flag=0;
                break;
            }
        
        }
        if(flag==1)
            printf("True");
        else
            printf("False");
    }

---
+++ Strictly_EVEN.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+    
+    int n, arr[100],i, flag=1;
+    
+    scanf("%d", &n);
+    
+    for(i=0;i<n;i++)
+        scanf("%d", &arr[i]);
+    for(i=0;i<n;i++){
+        
+        if(i%2==1 && arr[i]%2==0){
+            flag=0;
+            break;
+        }
+    
+    }
+    if(flag==1)
+        printf("True");
+    else
+        printf("False");
+}
